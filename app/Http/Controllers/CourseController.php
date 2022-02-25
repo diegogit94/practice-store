c25bd2b03c7e7e47802695c42f2c1ca74b0558e0
MrHyde <diego.ospinagp@gmail.com>
2022-02-24 21:12:26 -0500

    feat: created store method on CourseController and changed paginate order to index

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -16,7 +16,7 @@ class CourseController extends Controller
         //     ['name' => 'Javascript']
         // ];
 
-        $courses = Course::paginate(4);
+        $courses = Course::orderBy('id', 'desc')->paginate(4);
 
         return view('courses.index', compact('courses'));
     }
@@ -32,5 +32,34 @@ class CourseController extends Controller
         $course = Course::find($id);
 
         return view("/courses/show", compact('course'));
+    }
+
+    public function store(Request $request)
+    {
+        $course = new Course;
+
+        $course->name = $request->name;
+        $course->description = $request->description;
+        $course->category = $request->category;
+
+        $course->save();
+
+        return redirect(route('courses.show', $course->id)); // I could only put "course" without access to id attribute
+    }
+
+    public function edit(Course $course)
+    {
+        return view('courses.edit', compact('course'));
+    }
+
+    public function update(Request $request, Course $course)
+    {
+        $course->name = $request->name;
+        $course->description = $request->description;
+        $course->category = $request->category;
+
+        $course->update();
+
+        return redirect(route('courses.show', $course->id));
     }
 }
