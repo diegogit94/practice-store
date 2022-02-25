9e7c61e96dd3390f84d68daecacd2598ffe1101f
MrHyde <diego.ospinagp@gmail.com>
2022-02-24 21:13:01 -0500

    view: created form to create new courses

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -4,4 +4,27 @@
 
 @section('content')
     <h1>Create a course :)</h1>
+    <form action="{{ route('courses.store') }}" method="POST">
+        @csrf
+        <label>
+            Name:
+            <br>
+            <input type="text" name="name">
+        </label>
+        <br>
+        <label>
+            Description:
+            <br>
+            <textarea type="text" name="description" rows="5"></textarea>
+        </label>
+        <br>
+        <label>
+            Category:
+            <br>
+            <input type="text" name="category">
+        </label>
+        <br>
+        <br>
+        <button type="submit">Create Course</button>
+    </form>
 @endsection
