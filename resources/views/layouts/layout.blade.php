387770f4bb4da7b400f7a9e9dee74c6f9dbf0991
MrHyde <diego.ospinagp@gmail.com>
2022-02-28 22:16:00 -0500

    style: added style for active links on layout

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,10 +7,18 @@
     {{-- <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script> --}}
     <title>@yield('title', 'Demo')</title>
     @include('layouts.partials.header')
+
+    <style>
+        .active {
+            color: rosybrown;
+            font-weight: bold;
+        }
+    </style>
+
 </head>
 <body>
     @yield('content')
-    
+
     @include('layouts.partials.footer')
 </body>
 </html>
