a64788539ca3ef688805aabc9e48a31fc37b8a8f
MrHyde <diego.ospinagp@gmail.com>
2022-02-28 22:14:45 -0500

    feat: actived links in header navbar

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -2,9 +2,8 @@
     <h1>{{ config('app.name') }}</h1>
     <nav>
         <ul>
-            <li><a href="{{ route('home.index') }}">Home</a></li>
-            <li><a href="{{ route('courses.index') }}">Courses</a></li>
-            <li><a href="{{ route('courses.create') }}">Create a course</a></li>
+            <li><a href="{{ route('home.index') }}" class="{{ request()->routeIs('home.index') ? 'active' : '' }}">Home</a></li>
+            <li><a href="{{ route('courses.index') }}" class="{{ request()->routeIs('courses.*') ? 'active' : '' }}">Courses</a></li>
         </ul>
     </nav>
 </header>
