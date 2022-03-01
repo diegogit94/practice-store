52899422abc4344fe8e25ceffbf3622b6582e686
MrHyde <diego.ospinagp@gmail.com>
2022-02-28 22:14:57 -0500

    feat: active links on footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <ul>
-        <li><a href="{{ route('information.about') }}">About Us</a></li>
-        <li><a href="/about-us">Contact</a></li>
+        <li><a href="{{ route('information.about') }}" class="{{ request()->routeIs('information.about') ? 'active' : '' }}">About Us</a></li>
+        <li><a href="#">Contact</a></li>
     </ul>
 </footer>
