66add1464960127f62b81fa281ecdfe5c61f1335
MrHyde <diego.ospinagp@gmail.com>
2022-03-02 22:07:20 -0500

    feat: created basic view for mail

---
+++ resources/views/emails/contact-us.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Thanks for your opinion</h1>
+
+    <p>
+        Hi, we are {{ config('app.name') }}
+    </p>
+
+</body>
+</html>
